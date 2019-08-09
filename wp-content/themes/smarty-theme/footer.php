32626aedee38c459217dbfc8a1c2796596c92bec
petarsiderov <petar.siderov@avicenna4it.com>
2019-08-09 10:26:32 +0200

    added footer.php

--- wp-content/themes/smarty-theme/footer.php
+++ wp-content/themes/smarty-theme/footer.php
@@ -10,27 +10,74 @@
  */
 
 ?>
-
 	</div><!-- #content -->
+	<footer id="footer">
+				<div class="container">
+					<div class="row">
+						<div class="col-md-3"> 
+						<?php dynamic_sidebar( 'sidebar-2' ); ?>
+						<!-- footer-logo ---> 
+						</div><!-- col-md-3 -->
+						<div class="col-md-3">
+						
+							<!-- Latest Blog Post -->
+							
+							<ul class="footer-posts list-unstyled">
+								<?php dynamic_sidebar( 'sidebar-4' ); ?>
+							<!-- /Latest Blog Post -->
 
-	<footer id="colophon" class="site-footer">
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'newstoday24' ) ); ?>">
-				<?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'newstoday24' ), 'WordPress' );
-				?>
-			</a>
-			<span class="sep"> | </span>
-				<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'newstoday24' ), 'newstoday24', '<a href="https://www.linkedin.com/in/petar-siderov-78899a10b/">Petar Siderov</a>' );
-				?>
-		</div><!-- .site-info -->
-	</footer><!-- #colophon -->
+							</div><!-- col-md-3 ---> 
+							<div class="col-md-2">
+								<h2 class="widget-title"> Navigation </h2>
+								<?php 
+									wp_nav_menu( array(
+										'theme_location'   => 'footer-menu',
+										'container'        => 'ul',
+										'menu_class'  	   => 'footer-links list-unstyled',
+										'contaier'         => 'a'
+									));
+								?>
+								<!-- Links -->
+							</div><!-- col-md-2 --->
+						
+						<div class="col-md-4">
+							<?php dynamic_sidebar( 'sidebar-3' ); ?>
+						</div>
+					</div>
+				</div>
+				<div class="copyright">
+					<div class="container">
+						
+						&copy; All Rights Reserved, LTD <?php bloginfo('name'); ?> ,  <?php echo date('Y') ?> Author <?php  the_author(); ?>
+					</div>
+				</div>
+				</div> <!-- row -->
+	</footer><!-- footer -->
+	
+	
+
+		
+
 </div><!-- #page -->
 
 <?php wp_footer(); ?>
 
 </body>
 </html>
+<!-- JAVASCRIPT FILES -->
+<script>var plugin_path = '<?php bloginfo('stylesheet_directory'); ?>/assets/plugins/';</script>
+		<script src="<?php bloginfo('stylesheet_directory'); ?>/assets/plugins/jquery/jquery-3.3.1.min.js"></script>
+
+		<script src="<?php bloginfo('stylesheet_directory'); ?>/assets/js/scripts.js"></script>
+		
+			<!-- JAVASCRIPT FILES -->
+		<script>var plugin_path = '<?php bloginfo('stylesheet_directory'); ?>/assets/plugins/';</script>
+		<script src="<?php bloginfo('stylesheet_directory'); ?>/assets/plugins/jquery/jquery-3.3.1.min.js"></script>
+
+		<script src="<?php bloginfo('stylesheet_directory'); ?>/assets/js/scripts.js"></script>
+		
+	
+
+		<!-- SWIPER SLIDER -->
+		<script src="<?php bloginfo('stylesheet_directory'); ?>/assets/plugins/slider.swiper/dist/js/swiper.min.js"></script>
+		<script src="<?php bloginfo('stylesheet_directory'); ?>/assets/js/view/demo.swiper_slider.js"></script>
