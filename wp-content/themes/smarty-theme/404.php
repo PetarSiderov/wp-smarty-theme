23c346c8e30d210bdc922c45589f0c030ff423e5
petarsiderov <petar.siderov@avicenna4it.com>
2019-08-12 15:06:14 +0200

    added new 404.php from smarty theme

--- wp-content/themes/smarty-theme/404.php
+++ wp-content/themes/smarty-theme/404.php
@@ -9,25 +9,55 @@
 
 get_header();
 ?>
+<section class="page-header dark">
+				<div class="container">
 
-		
-		
-<section>
-		<div class="row">
-		<div class="col-md-8">
-		<h1 class="page-header fw-300 text-center">
-				<strong class="page-title"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'newstoday24' ); ?></strong>
-		</h1>	
-		</div>
-		<div class="col-md-3">
-			<?php 
-				get_sidebar();
-			?>
-		</div>
+					<h1><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'newstoday24' ); ?></h1>
 
-		</div>
+				</div>
+			</section>
+		
+			<section class="section-xlg">
+				<div class="container">
+					
+					<div class="row">
+
+						<div class="col-md-6 col-sm-6 hidden-xs-down">
+							
+							<div class="error-404">
+								404
+							</div>
+						
+						</div>
+
+						<div class="col-md-6 col-sm-6">
+						
+							<h3 class="m-0">Sorry, <strong>The page you requested can not be found!</strong></h3>
+							<p class="mt-0 fs-20 font-lato text-muted">Please, search again using more specific keywords.</p>
+
+							<!-- INLINE SEARCH -->
+							<div class="inline-search clearfix mb-60">
+								<form action="" method="get" class="widget_search">
+									<input type="search" placeholder="Search..." id="s" name="s" class="serch-input">
+									<button type="submit">
+										<i class="fa fa-search"></i>
+									</button>
+									<div class="clear"></div>
+								</form>
+							</div>
+							<!-- /INLINE SEARCH -->
+
+							<div class="divider mb-0"><!-- divider --></div>
+
+							<a class="fs-16 font-lato" href="/index.php"><i class="glyphicon glyphicon-menu-left mr-10 fs-12"></i></a>
+
+						</div>
+
+					</div>
+					
+				</div>
+			</section>
 	
-</section>		
 
 
 <?php
--- wp-content/themes/smarty-theme/page-home.php
+++ wp-content/themes/smarty-theme/page-home.php
@@ -8,5 +8,5 @@ $thumbnail_url = wp_get_attachment_url( get_post_thumbnail_id ( $post->ID ) );
 
 ?>
         <h1> <?php the_title(); ?> </h1>
-
+        
 <?php get_footer()?>
